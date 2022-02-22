5d491f4fb4776a771c8db68c1d0b820f051aff71
Marcin Andrzejczak <amostori@op.pl>
2022-02-22 15:35:26 +0100

    gunicorn add

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # ⌥ + ⇧ multi cursor
+# pip freeze > requirements.txt
 from flask import Flask, render_template
 
 app = Flask("__name__")
@@ -10,4 +11,4 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
